a795aa2dc7236ed69da6f3c0f8bddc21d5ed770c
Jorbnc <67754592+Jorbnc@users.noreply.github.com>
2022-05-26 00:10:26 -0500

    update

---
+++ src/Entities/Agents.jl
@@ -0,0 +1,32 @@
+module agents
+
+# --------------------------------------------------------------------------------
+
+# maybe I should import an existing package for this...
+abstract type Node end
+
+# --------------------------------------------------------------------------------
+
+# gotta import Functions/Actions.jl
+# (make a quick diagram for import/using logic and add it to the Julia cheatsheet)
+
+# ...anyway, follow this
+struct Current_node <: Node
+    name::String # (must be optional)
+    # See <.../Functions>
+    actions::Array{Union{Missing, Actions}}
+    # (or should I simply use 
+    #   > actions::Array{Union{Missing, Functions}}
+    # for this ?)
+end
+
+struct prev_node <: Node # an also create an next_node (ofc)
+    name::String # (must be optional)
+    parent::Current_node
+    actions::Array{Union{Missing, Function}}
+end
+
+# --------------------------------------------------------------------------------
+
+end # Module entities
+
