794541868fdb5482f3352312866a7423f0fce134
Jorbnc <67754592+Jorbnc@users.noreply.github.com>
2022-05-26 12:47:37 -0500

    cleaning

--- src/Entities/Agents.jl
+++ src/Entities/Agents.jl
@@ -3,30 +3,16 @@ module Agents
 # --------------------------------------------------------------------------------
 
 # maybe I should import an existing package for this...
-abstract type Node end
-
-# --------------------------------------------------------------------------------
 
 # gotta import Functions/Actions.jl
 # (make a quick diagram for import/using logic and add it to the Julia cheatsheet)
 
-# ...anyway, follow this
-struct Current_node <: Node
-    name::String # (must be optional)
-    # See <.../Functions>
-    actions::Array{Union{Missing, Actions}}
-    # (or should I simply use 
-    #   > actions::Array{Union{Missing, Functions}}
-    # for this ?)
+abstract type Company end
+
+struct Manufacturer <: Company
+    name::Union{Missing, String}
+    suppliers::Array{Union{Missing, Company}}
+    customers::Array{Union{Missing, Company}}
 end
 
-struct prev_node <: Node # an also create an next_node (ofc)
-    name::String # (must be optional)
-    parent::Current_node
-    actions::Array{Union{Missing, Function}}
-end
-
-# --------------------------------------------------------------------------------
-
-end # Module Entities
-
+end # Module Agents
--- src/SupplyChainModels.jl
+++ src/SupplyChainModels.jl
@@ -1,29 +1,7 @@
 module SupplyChainModels
 
-include("EOQ.jl")
-include("entities.jl")
-
-a = EOQ.eoq(ct=500, c=50, demand=2000, ce=50*0.25)
-a.Q
-
-# Costs
-a.HoldingCost
-a.OrderingCost
-a.TRC()
-a.TC()
-
-# Policy
-a.policy()
-a.policy_for("Q", 500)
-a.policy_for("T", 3.0)
-
-for i in (1:10^4)
-    c = rand(45:55)
-    b = EOQ.eoq(ct=rand(450:550), c=c, demand=rand(1950:2050), ce=c*0.25)
-    b.TC()
-end
+include("Optimization/EOQ.jl")
+include("Entities/Agents.jl")
 
 end # Module EOQ
-
-# No puedo correr los códigos acá (fuera del módulo) ---> Investigar y arreglar
 
