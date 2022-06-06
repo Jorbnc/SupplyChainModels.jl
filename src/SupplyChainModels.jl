421ca41dcaa0ec620b42643da3b86a879fa3b1a1
Jorbnc <67754592+Jorbnc@users.noreply.github.com>
2022-06-05 21:14:09 -0500

    fixed modules functionality

--- src/SupplyChainModels.jl
+++ src/SupplyChainModels.jl
@@ -3,8 +3,8 @@ module SupplyChainModels
 using Reexport
 
 # Order of included modules is important
-include("Flow_Units/FlowUnits.jl") # Doesn't not depend on any module (yet)
-include("Entities/Agents.jl") # Depenens on FlowUnits
+include("Flow_Units/FlowUnits.jl") # Doesn't depend on any module (yet)
+include("Entities/Agents.jl") # Depends on FlowUnits
 include("Actions/Source.jl") # Depends on Agents, FlowUnits
 
 @reexport using .FlowUnits
